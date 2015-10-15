32ecfad28f1f169a340d7aad3fadaee1221616ff
Ben MacNeill <ben.macneill@gmail.com>
2015-10-15 09:52:56 -0400

    update all global nav bars to reflect new about focus

--- public/javascripts/global_nav_internal_bs.js
+++ public/javascripts/global_nav_internal_bs.js
@@ -1,9 +1,8 @@
 var globalNav = '<ul class="nav navbar-nav">\
-      <li><a href="http://www.extension.org" title="A broad range of content from a network of experts at universities across America.">eXtension.org</a></li>\
-      <li><a href="http://about.extension.org/" title="A starting place for all things eXtension.">About</a></li>\
+      <li><a href="http://about.extension.org" title="eXtension - For Extension Professionals and the Public They Serve">eXtension.org</a></li>\
       <li><a href="https://ask.extension.org" title="One-to-one answers from a network of experts at universities across America.">Ask an Expert</a></li>\
-      <li><a href="http://blogs.extension.org/" title="eXtension Blogs">Blogs</a></li>\
       <li><a href="http://campus.extension.org/ " title="Online courses developed by Cooperative Extension and available to anyone.">Campus</a></li>\
+      <li><a href="http://www.extension.org/" title="A starting place for all things eXtension.">Resources</a></li>\
       <li><a href="https://learn.extension.org/" title="Professional development sessions on a variety of Extension topics.">Learn</a></li>\
       <li><a href="https://search.extension.org" title="One search from hundreds of Cooperative Extension sites.">Search</a></li>\
       <li><a href="http://create.extension.org/" title="A collaborative development of resources about the eXtension initiative: news, governance, and projects.">Create</a></li>\
--- public/javascripts/global_nav_public.js
+++ public/javascripts/global_nav_public.js
@@ -18,11 +18,10 @@ ul.extension_global_nav li a:hover {color: #fff;}\
 <ul class="extension_global_nav public_nav noprint">\
   <li class="group"><span>Public sites</span>\
     <ul>\
-      <li><a href="http://www.extension.org" title="A broad range of content from a network of experts at universities across America.">eXtension.org</a></li>\
-      <li><a href="http://about.extension.org/" title="A starting place for all things eXtension.">About</a></li>\
+      <li><a href="http://about.extension.org" title="eXtension - For Extension Professionals and the Public They Serve">eXtension.org</a></li>\
       <li><a href="https://ask.extension.org" title="One-to-one answers from a network of experts at universities across America.">Ask an Expert</a></li>\
-      <li><a href="http://blogs.extension.org/" title="eXtension Blogs">Blogs</a></li>\
       <li><a href="http://campus.extension.org/ " title="Online courses developed by Cooperative Extension and available to anyone.">Campus</a></li>\
+      <li><a href="http://www.extension.org/" title="A starting place for all things eXtension.">Resources</a></li>\
       <li><a href="https://learn.extension.org/" title="Professional development sessions on a variety of Extension topics.">Learn</a></li>\
       <li><a href="https://search.extension.org" title="One search from hundreds of Cooperative Extension sites.">Search</a></li>\
     </ul>\
