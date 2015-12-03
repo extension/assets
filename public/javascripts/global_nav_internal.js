620d4f765ec7f3cd2902408805d081aadd884036
Ben MacNeill <ben.macneill@gmail.com>
2015-12-03 16:16:27 -0500

    more css tweak

--- public/javascripts/global_nav_internal.js
+++ public/javascripts/global_nav_internal.js
@@ -11,7 +11,7 @@ ul.extension_global_nav li {display:inline; padding:0;}\
 ul.extension_global_nav .last {border:0;}\
 ul.extension_global_nav li a {padding:.2em .5em; color:#999; text-decoration: none;}\
 ul.extension_global_nav li a:hover {color: #fff;}\
-.global_announcement {background: #f5f5f5; color: #888; padding:.55em 1em .1em; margin: -5px -26px 20px; text-align:center; font-size:14px;}\
+.global_announcement {background: #f5f5f5; color: #888; padding:.55em 1em .1em; margin: 0; text-align:center; font-size:14px;}\
 .global_announcement a {color: #f47B28;font-weight:normal;}\
 </style>\
 \
@@ -37,7 +37,7 @@ ul.extension_global_nav li a:hover {color: #fff;}\
 </ul>\
 \
 \
-<div class="global_announcement"><p><a href="https://extension.org/extension-i-three-event-2016/?utm_source=web&utm_medium=global_banner&utm_campaign=ithree_event">Coming March 2016...eXtension i-Three Event...San Antonio</a>.</p></div>';
+<div class="global_announcement"><p><a href="https://extension.org/extension-i-three-event-2016/?utm_source=web&utm_medium=global_banner&utm_campaign=ithree_event">Coming March 2016...eXtension i-Three Event...San Antonio</a></p></div>';
 (function() {
     document.write(globalNav);
 })();
