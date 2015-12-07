3a5b10020d36806ef709631951545cebb2aa0588
Ben MacNeill <ben.macneill@gmail.com>
2015-12-07 15:36:42 -0500

    internal shortcuts bar

---
+++ public/javascripts/global_shortcuts_bar_internal.js
@@ -0,0 +1,27 @@
+var globalNav = '<style media="screen" type="text/css">\
+body {margin:0;}\
+ul.extension_global_nav {text-align:right; margin:0; padding:.25em 12px .35em; background:#C2C6CC;}\
+ul.extension_global_nav li {font: normal 13px/20px \'Helvetica Neue\', Helvetica, Arial, sans-serif; display:inline; padding:0;}\
+ul.extension_global_nav li a {padding:.2em .5em; color:#5A6782; text-decoration: none;}\
+ul.extension_global_nav li a:hover {color: #1A294A;}\
+ul.extension_global_nav .nav-name {color:#555; font-weight:bold;}\
+.global_announcement {background: #81858b; color: #ccc; padding:5px 1em 5px; margin: 0; text-align:center; font: normal 13px/20px \'Helvetica Neue\', Helvetica, Arial, sans-serif;}\
+.global_announcement p {margin:0;}\
+.global_announcement a {color: #ddd; font-weight:normal; text-decoration: none;}\
+</style>\
+\
+<div class="global_announcement"><p><a href="https://extension.org/extension-i-three-event-2016/?utm_source=web&utm_medium=global_banner&utm_campaign=ithree_event">Coming March 2016...eXtension i-Three Event...San Antonio</a></p></div>\
+<ul class="extension_global_nav public_nav noprint">\
+  <li class="nav-name">shortcuts:</li>\
+  <li><a href="https://extension.org/" title="eXtension - For Extension Professionals and the Public They Serve">eXtension.org</a></li>\
+  <li><a href="https://ask.extension.org" title="One-to-one answers from a network of experts at universities across America.">Ask an Expert</a></li>\
+  <li><a href="http://campus.extension.org/" title="Online courses developed by Cooperative Extension and available to anyone.">Campus</a></li>\
+  <li><a href="http://articles.extension.org/" title="Issues. Innovation. Impact.">Resources</a></li>\
+  <li><a href="https://learn.extension.org/" title="Professional development sessions on a variety of Extension topics.">Learn</a></li>\
+  <li><a href="https://search.extension.org" title="One search from hundreds of Cooperative Extension sites.">Search</a></li>\
+  <li><a href="http://create.extension.org/" title="A collaborative development of resources about the eXtension initiative: news, governance, and projects.">Create</a></li>\
+  <li><a href="https://people.extension.org/" title="Manage your eXtension profile, find colleagues, create and join communities.">People</a></li>\
+</ul>';
+(function() {
+    document.write(globalNav);
+})();
