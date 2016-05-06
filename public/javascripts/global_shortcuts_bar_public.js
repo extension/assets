a32678b547f7da9c9da334ebf128fd62f4283ca3
Ben MacNeill <ben.macneill@gmail.com>
2016-05-06 17:13:53 -0400

    Update banner

--- public/javascripts/global_shortcuts_bar_public.js
+++ public/javascripts/global_shortcuts_bar_public.js
@@ -14,7 +14,7 @@ ul.extension_global_nav .nav-name {color:#555; font-weight:bold;}\
   ul.extension_global_nav > li > a {padding: 11px 5px !important; display:block;}\
 }</style>\
 \
-<div class="global_announcement"><p>Join us for this Wednesday\'s eXtension webinar on ​<a href="https://learn.extension.org/events/2621">New Opportunities for Extension Professionals</a></p></div>\
+<div class="global_announcement"><p><a href="https://learn.extension.org/events/2621">View May 4 Webinar on Emerging Opportunities for Extension Professionals</a></p></div>\
 <ul class="extension_global_nav public_nav noprint">\
   <li class="nav-name">shortcuts:</li>\
   <li><a href="https://extension.org/" title="eXtension - For Extension Professionals and the Public They Serve">eXtension.org</a></li>\
